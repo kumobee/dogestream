5511b6ab99eb455aac9609c0751f278f7408dd3d
Fernando Freire <dogonthehorizon@gmail.com>
2014-01-30 14:04:12 -0800

    wow routes!

--- test/video.js
+++ test/video.js
@@ -4,7 +4,7 @@ var request = require('supertest')
   , app = require('../app');
 
 describe('GET /video', function() {
-    var id, video;
+    var id, video, notDogeVideo, dogeVideos;
 
     video = {
         title: 'Teach Me How To Doge',
@@ -13,11 +13,21 @@ describe('GET /video', function() {
         duration: '00:03:01',
         category: 'doge'
 
-    }
+    };
+
+    notDogeVideo = {
+        title: 'Something That Isn\'t Doge Therefore Less Important',
+        description: 'You won\'t learn how to doge here...',
+        path: 'some/stupid/path',
+        duration: '00:00:00',
+        category: 'NotDoge'
+    };
+
+    dogeVideos = [video, notDogeVideo];
 
     beforeEach(function(done) {
         mongoose.connection.collections['videos'].drop(function(err, docs) {
-            mongoose.connection.collections['videos'].insert(video, function(err, docs) {
+            mongoose.connection.collections['videos'].insert(dogeVideos, function(err, docs) {
                 id = docs[0]._id;
                 done();
             });
@@ -32,16 +42,34 @@ describe('GET /video', function() {
                 .expect(200)
                 .end(function(err, res) {
                     var result = JSON.parse(res.text)[0];
-
-                    assert.equal(result._id, id)
+                    assert.equal(result._id, id);
                     for(var key in video) {
                         if(video.hasOwnProperty(key)) {
                             assert.equal(result[key], video[key]);
                         }
                     }
+
+                    done();
                 });
 
-            done();
+
+        });
+    });
+
+    describe('when requesting resource /video/category', function() {
+        it('should return an array of videos that match category', function(done) {
+            request(app)
+                .get('/video/doge')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    var result = JSON.parse(res.text)[0];
+
+                    assert.equal(result._id, id);
+                    assert.equal(result.category, video.category);
+
+                    done();
+                });
         });
     });
 });
